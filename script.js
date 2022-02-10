97afe5b91a1817d67f26d77fd0b8fff622c00d8c
krash76 <krash76@mail.ru>
2022-02-10 22:19:08 +0100

    script.js changed

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 const btn = document.getElementById("btn");
 const input = document.querySelector("input[type='text']");
 const square = document.querySelector("#square");
+const circle = document.querySelector("#circle");
 const circleBtn = document.querySelector("#e_btn");
+const inputRange = document.querySelector("input[type='range']");
+
 circleBtn.style.display = "none";
 
 btn.addEventListener("click", () => {
   square.style.backgroundColor= input.value; 
 });
 
+inputRange.addEventListener("input", () => {
+  circle.style.width = inputRange.value + "%";
+  circle.style.height = inputRange.value + "%";
+});
 
 
 
